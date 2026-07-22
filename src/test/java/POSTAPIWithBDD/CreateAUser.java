f0cfb23b1f4b4d547afd6e9d14eea83c3ffbc570
ponraj-hub <ponraj8785@gmail.com>
2026-07-22 12:35:00 +0530

    Random Email Generation With JSON

--- src/test/java/POSTAPIWithBDD/CreateAUser.java
+++ src/test/java/POSTAPIWithBDD/CreateAUser.java
@@ -7,6 +7,11 @@ import io.restassured.http.ContentType;
 
 import static io.restassured.RestAssured.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class CreateAUser {
 	
 	public String getRandomEmailId() {
@@ -14,7 +19,7 @@ public class CreateAUser {
 	}
 	
 	@Test
-	public void CreateUser() {
+	public void CreateUserWithJSONStringTest() {
 		
 		RestAssured.baseURI="https://gorest.co.in/";
 		String emailId= getRandomEmailId();
@@ -36,5 +41,49 @@ public class CreateAUser {
 		.statusCode(201);	
 	}
 	
+	
+	@Test
+	public void CreateUserWithJSONFileTest() {
+		
+		RestAssured.baseURI="https://gorest.co.in/";
+
+		given().log().all()
+		.contentType(ContentType.JSON)
+		.header("Authorization","Bearer c9debfcd908f8b4e46428181b1301810c2e79439bfdc0c0c47b9b089e8cdfcbb")
+	    .body(new File("./src/test/resources/JSON/user.json"))
+	    
+		.when()
+		.post("public/v2/users")
+		
+		.then().log().all()
+		.assertThat()
+		.statusCode(201);	
+	}
+	
+	
+
+	@Test
+	public void CreateUserWithJSONEmailReplacementTest() throws IOException {
+		
+		RestAssured.baseURI="https://gorest.co.in/";
+		String emailId= getRandomEmailId();
+		
+		//Convert the JSON File Content to String
+		
+		String rawFile=new String(Files.readAllBytes(Paths.get(".\\src\\test\\resources\\JSON\\user.json")));
+		String updatedFile=rawFile.replace("{{email}}", emailId);
+
+		given().log().all()
+		.contentType(ContentType.JSON)
+		.header("Authorization","Bearer c9debfcd908f8b4e46428181b1301810c2e79439bfdc0c0c47b9b089e8cdfcbb")
+	    .body(updatedFile)
+	    
+		.when()
+		.post("public/v2/users")
+		
+		.then().log().all()
+		.assertThat()
+		.statusCode(201);	
+	}
 	
 }
