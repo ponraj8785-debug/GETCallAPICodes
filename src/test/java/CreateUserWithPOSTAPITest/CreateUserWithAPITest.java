0f0ba44b3887ddb0e269ab932b2708e0a6875832
ponraj-hub <ponraj8785@gmail.com>
2026-08-11 14:07:33 +0530

    Create and Get the user

--- src/test/java/CreateUserWithPOSTAPITest/CreateUserWithAPITest.java
+++ src/test/java/CreateUserWithPOSTAPITest/CreateUserWithAPITest.java
@@ -1,6 +1,7 @@
 package CreateUserWithPOSTAPITest;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,6 +18,7 @@ public class CreateUserWithAPITest {
 		return "apiautomation" + System.currentTimeMillis() + "@opencart.com";
 	}
 
+	// Step 1 :: Create a user
 	@Test
 	public void CreateUserTest() throws IOException {
 
@@ -28,7 +30,6 @@ public class CreateUserWithAPITest {
 		String rawFile = new String(Files.readAllBytes(Paths.get(".\\src\\test\\resources\\JSON\\user.json")));
 		String updatedFile = rawFile.replace("{{email}}", emailId);
 
-		//Step 1 :: Create a user
 		int userID = given().log().all().contentType(ContentType.JSON)
 				.header("Authorization", "Bearer c9debfcd908f8b4e46428181b1301810c2e79439bfdc0c0c47b9b089e8cdfcbb")
 				.body(updatedFile)
@@ -38,6 +39,20 @@ public class CreateUserWithAPITest {
 				.then().log().all().assertThat().statusCode(201).extract().path("id");
 
 		System.out.println("User Id is " + userID);
-	}
+		
+		System.out.println("----------GET CALL METHOD---------------");
 
+		// Step 2 :: Get user by the same user id
+
+		given().log().all().header("Authorization", "Bearer c9debfcd908f8b4e46428181b1301810c2e79439bfdc0c0c47b9b089e8cdfcbb")
+				.when().get("/public/v2/users/" + userID)
+				.then()
+				.assertThat()
+				.statusCode(200)
+				.and()
+				.body("id", equalTo(userID))
+				.body("name", equalTo("Ethel_Strosin26"))
+				.body("email", equalTo(emailId));
+
+	}
 }
