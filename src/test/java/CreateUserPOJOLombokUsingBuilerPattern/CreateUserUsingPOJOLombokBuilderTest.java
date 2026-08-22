9b333778d1f22967e6c107fdecf1f829424ee69c
ponraj-hub <ponraj8785@gmail.com>
2026-08-22 07:43:06 +0530

    second commit

--- src/test/java/CreateUserPOJOLombokUsingBuilerPattern/CreateUserUsingPOJOLombokBuilderTest.java
+++ src/test/java/CreateUserPOJOLombokUsingBuilerPattern/CreateUserUsingPOJOLombokBuilderTest.java
@@ -51,7 +51,8 @@ public class CreateUserUsingPOJOLombokBuilderTest {
 
 		given().log().all()
 				.header("Authorization", "Bearer c9debfcd908f8b4e46428181b1301810c2e79439bfdc0c0c47b9b089e8cdfcbb")
-				.when().get("/public/v2/users/" + userID)
+				.when()
+				.get("/public/v2/users/" + userID)
 				.then().log().all()
 				 .assertThat()
 				  .statusCode(200)
