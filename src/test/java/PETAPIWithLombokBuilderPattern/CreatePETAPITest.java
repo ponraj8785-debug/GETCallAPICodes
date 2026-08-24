2c8acee3925e5f671f0263bd2864792e37092a30
ponraj-hub <ponraj8785@gmail.com>
2026-08-24 07:29:13 +0530

    Pet Using Lombok Builder Patter

--- src/test/java/PETAPIWithLombokBuilderPattern/CreatePETAPITest.java
+++ src/test/java/PETAPIWithLombokBuilderPattern/CreatePETAPITest.java
@@ -1,5 +1,99 @@
 package PETAPIWithLombokBuilderPattern;
 
-public class CreatePETAPITest {
+import org.testng.annotations.Test;
 
+import PETAPIWithLombokBuilderPattern.Pet.Category;
+import PETAPIWithLombokBuilderPattern.Pet.Tag;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CreatePETAPITest {
+	
+	
+	@Test
+	public void createPetAPITest() {
+		
+		RestAssured.baseURI="https://petstore.swagger.io";
+		
+		//Create the object f the pet class
+		
+		Pet.Category category=new Category(111, "dog");
+		
+		List<String>photoURLs=  Arrays.asList
+				("https://www.photo123.com","https://www.photo123.com","https://www.photo123.com");
+		
+		Pet.Tag tag1=new Pet.Tag(101, "White");
+		Pet.Tag tag2= new Pet.Tag(102, "Black");
+		
+		List<Tag>tags= Arrays.asList(tag1,tag2);
+		
+		Pet pet= new Pet(001, "dogs", "available", photoURLs, tags, category);
+		
+		
+		given().log().all()
+		.contentType(ContentType.JSON)
+		.body(pet)
+		.when()
+		.post("/v2/pet")
+		.then()
+		.log().all()
+	    .assertThat()
+	      .statusCode(200);
+		
+	}
+
+	
+	@Test
+	public void createPetAPITestUsingBuilderPattern() {
+		
+	RestAssured.baseURI="https://petstore.swagger.io";
+		
+	Pet.Category category= new Pet.Category.CategoryBuilder()
+		.id(1)
+		.name("dog")
+		.build();
+		
+	List<String>photoURLs=  Arrays.asList
+			("https://www.photo123.com","https://www.photo123.com","https://www.photo123.com");
+	
+    Pet.Tag tag1=new Pet.Tag.TagBuilder()
+	    .id(101)
+	    .name("dogs")
+	    .build();
+    Pet.Tag tag2=new Pet.Tag.TagBuilder()
+    	.id(102)
+    	.name("dogers")
+    	.build();
+    
+    List<Tag>tags= Arrays.asList(tag1,tag2);
+    
+    Pet pet=new Pet.PetBuilder()
+    		.id(1)
+    		.name("Rogers")
+    		.status("Available")
+    		.photoUrls(photoURLs)
+    		.tag(tags)
+    		.category(category)
+    		.build();
+    
+
+	given().log().all()
+	.contentType(ContentType.JSON)
+	.body(pet)
+	.when()
+	.post("/v2/pet")
+	.then()
+	.log().all()
+    .assertThat()
+      .statusCode(200);
+	
+    
+	}
 }
+	
